c42dfed31b7445546499ff29fea1ad31ab768e57
Heath Holuska <heath.holuska@gmail.com>
2026-03-29 21:11:11 -0500

    Refactor Database class for improved readability

--- api/config/Database.php
+++ api/config/Database.php
@@ -1,6 +1,7 @@
 <?php
 class Database
 {
+
     private $host = "dpg-d72tg895pdvs73f3tri0-a.oregon-postgres.render.com"; 
     private $port = "5432";
     private $db_name = "quotesdb_e4p5";
@@ -12,9 +13,12 @@ class Database
     {
         $this->conn = null;
         try {
+
             $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
             
             $this->conn = new PDO($dsn, $this->username, $this->password);
+            
+
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
         } catch (PDOException $e) {
