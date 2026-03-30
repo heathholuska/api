0c6a6f2b4e55f19c7d846cc307b6bab42d473fbd
Heath Holuska <heath.holuska@gmail.com>
2026-03-29 22:08:30 -0500

    Fix header formatting and clean up delete.php

--- api/post/quote/delete.php
+++ api/post/quote/delete.php
@@ -3,8 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
-    Access-Control-Allow_methods, Authorization, X-Requested-with');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include_once '../../config/Database.php';
 include_once '../../models/Post.php';
 
