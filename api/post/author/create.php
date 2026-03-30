d60346cb900346f5254b62d2aa042e2dc75f6a82
Heath Holuska <heath.holuska@gmail.com>
2026-03-30 12:37:55 -0500

    Fix include path for Author model in create.php

--- api/post/author/create.php
+++ api/post/author/create.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
-include_once '../../models/Author.php';
+include_once 'Author.php';
 
 $database = new Database();
 $db = $database->connect(); // Fixed connection method
