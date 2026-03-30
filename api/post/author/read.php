41140f20074289b96bb810d142ecd4ef8cbe5e5a
Heath Holuska <heath.holuska@gmail.com>
2026-03-30 12:44:40 -0500

    Refactor read.php to use Author model

--- api/post/author/read.php
+++ api/post/author/read.php
@@ -5,15 +5,15 @@ header('Content-Type: application/json');
 
 // include
 include_once '../../config/Database.php';
-include_once '../../models/Post.php';
+include_once 'Author.php';
 
 // Instantiate DB & Connect
 $database = new Database();
 $db = $database->connect();
 
 // Instantiate post object
-$post = new Post($db);
-$result = $post->read();
+$author = new Author($db);
+$result = $author->read();
 $num = $result->rowCount();
 
 // Check if there are any posts
