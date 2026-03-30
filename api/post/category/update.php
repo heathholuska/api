6951afc0742e1aa1b084c2e3d16d6e9e117a30fd
Heath Holuska <heath.holuska@gmail.com>
2026-03-29 22:07:57 -0500

    Fix header formatting and clean up update.php

--- api/post/category/update.php
+++ api/post/category/update.php
@@ -4,8 +4,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
-     Access-Control-Allow_methods, Authorization, X-Requested-with');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include_once '../../config/Database.php';
 include_once '../../models/Post.php';
 
