1d95a7d485d3f9d103421eba9aa6b4af60b10f48
Heath Holuska <heath.holuska@gmail.com>
2026-03-29 22:13:15 -0500

    Add Category class for category management

---
+++ api/post/category/Category.php
@@ -0,0 +1,63 @@
+<?php
+class Category {
+    // Database stuff
+    private $conn;
+    private $table = 'categories';
+
+    // Properties (Matches the database schema)
+    public $id;
+    public $category;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+        $query = 'SELECT id, category FROM ' . $this->table . ' ORDER BY id ASC';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function read_single() {
+        $query = 'SELECT id, category FROM ' . $this->table . ' WHERE id = ? LIMIT 1';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            $this->category = $row['category'];
+        }
+    }
+
+    public function create() {
+        $query = 'INSERT INTO ' . $this->table . ' (category) VALUES (:category)';
+        $stmt = $this->conn->prepare($query);
+        $this->category = htmlspecialchars(strip_tags($this->category));
+        $stmt->bindParam(':category', $this->category);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    public function update() {
+        $query = 'UPDATE ' . $this->table . ' SET category = :category WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $this->category = htmlspecialchars(strip_tags($this->category));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $stmt->bindParam(':category', $this->category);
+        $stmt->bindParam(':id', $this->id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    public function delete() {
+        $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $stmt->bindParam(':id', $this->id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+}
+?>
