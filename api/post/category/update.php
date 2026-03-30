286ec2dc2e5cf2cb7631e32ba9fc482d9a233a13
Heath Holuska <heath.holuska@gmail.com>
2026-03-30 12:52:16 -0500

    Refactor update.php to use Category model

--- api/post/category/update.php
+++ api/post/category/update.php
@@ -6,14 +6,14 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include_once '../../config/Database.php';
-include_once '../../models/Post.php';
+include_once 'Category.php';
 
 // Instantiate DB & Connect
 $database = new Database();
 $db = $database->connect();
 
 // Instantiate Blog Post Object
-$post = new Post($db);
+$category = new Category($db);
 
 // Get raw posted data
 $data = json_decode(file_get_contents("php://input"));
@@ -21,14 +21,14 @@ $data = json_decode(file_get_contents("php://input"));
 
 // Set ID to UPDATE
 
-$post->id = $data->id;
+$category->id = $data->id;
 
-$post->quote = $data->quote ?? null;
-$post->author_id = $data->author_id ?? null;
-$post->category_id = $data->category_id ?? null;
+$category->quote = $data->quote ?? null;
+$category->author_id = $data->author_id ?? null;
+$category->category_id = $data->category_id ?? null;
 
 //Update Post
-if ($post->update()) {
+if ($category->update()) {
     echo json_encode(
         array('message' => 'Post Updated')
     );
