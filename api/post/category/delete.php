2d4df18f4f908b7e427bd54dcde7f21289ef287c
Heath Holuska <heath.holuska@gmail.com>
2026-03-30 12:53:11 -0500

    Refactor delete.php to handle category deletion

--- api/post/category/delete.php
+++ api/post/category/delete.php
@@ -6,14 +6,14 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include_once '../../config/Database.php';
-include_once '../../models/Post.php';
+include_once 'Category.php';
 
 // Instantiate DB & Connect
 $database = new Database();
 $db = $database->connect();
 
 // Instantiate Blog Post Object
-$post = new Post($db);
+$category = new Category($db);
 
 // Get raw posted data
 $data = json_decode(file_get_contents("php://input"));
@@ -21,11 +21,11 @@ $data = json_decode(file_get_contents("php://input"));
 
 // Set ID to UPDATE
 
-$post->id = $data->id;
+$category->id = $data->id;
 
 
 //DELETE Post
-if ($post->delete()) {
+if ($category->delete()) {
     echo json_encode(
         array('message' => 'Post Deleted')
     );
