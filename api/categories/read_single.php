114a0943aa62f8ba09134968ff2c9bde60e09c73
Heath Holuska <heath.holuska@gmail.com>
2026-03-31 21:22:40 +0000

    Minor API JSON output changes

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -15,10 +15,13 @@ $category->id = isset($_GET['id']) ? $_GET['id'] : die();
 $category->read_single();
 
 // Create array
-$post_arr = array(
-    'id' => $category->id,
-    'category' => $category->category
-);
 
-// Make JSON
-echo (json_encode($post_arr));
+if ($category->category != null) {
+    $post_arr = array(
+        'id' => $category->id,
+        'category' => $category->category
+    );
+    echo json_encode($post_arr);
+} else {
+    echo json_encode(array('message' => 'category_id Not Found'));
+}
